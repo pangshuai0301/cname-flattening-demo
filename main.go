ebb4b48446e60b5db8b2670207c7beb3e1657f9e
庞帅 <ps-strive@pangshuaideMacBook-Pro.local>
2018-11-22 14:23:00 +0800

    fix:code format

--- main.go
+++ main.go
@@ -1,151 +1,151 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net"
-	"net/http"
-        "net/url"
-	"os"
-	"strings"
-	"errors"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net"
+    "net/http"
+    "net/url"
+    "os"
+    "strings"
+    "errors"
 )
 
 type Domain struct {
-	Dnspod_ns	[]string	`json:"dnspod_ns"`
-	Ext_status	string		`json:"ext_status"`
-	Grade		string		`json:"grade"`
-        Id		int		`json:"id"`
-	Min_ttl		int		`json:"min_ttl"`
-	Name		string		`json:"name"`
-	Owner		string		`json:"owner"`
-	Punycode	string		`json:"punycode"`
-	Status		string		`json:"status"`
-	Ttl		int		`json:"ttl"`
+    Dnspod_ns   []string    `json:"dnspod_ns"`
+    Ext_status  string      `json:"ext_status"`
+    Grade       string      `json:"grade"`
+    Id          int         `json:"id"`
+    Min_ttl     int         `json:"min_ttl"`
+    Name        string      `json:"name"`
+    Owner       string      `json:"owner"`
+    Punycode    string      `json:"punycode"`
+    Status      string      `json:"status"`
+    Ttl         int         `json:"ttl"`
 }
 
 type Info struct {
-	Record_total	string		`json:"record_total"`
-	Records_sum	string		`json:"records_sum"`
-	Sum_domains	string		`json:"sub_domains"`
+    Record_total    string      `json:"record_total"`
+    Records_sum     string      `json:"records_sum"`
+    Sum_domains     string      `json:"sub_domains"`
 }
 
 type Record struct {
-	Enable		string		`json:"enabled"`
-	Hold		string		`json:"hold"`
-	Id		string		`json:"id"`
-	Line		string		`json:"line"`
-	Line_id		string		`json:"line_id"`
-	Monitor_status	string		`json:"monitor_status"`
-	Mx		string		`json:"mx"`
-	Name		string		`json:"name"`
-	Remark		string		`json:"remake"`
-	Status		string		`json:"status"`
-	Ttl		string		`json:"ttl"`
-	Type		string		`json:"type"`
-	Updated_on	string		`json:"updated_on"`
-	Use_aqb		string		`json:"use_aqb"`
-	Value		string		`json:"value"`
-	Weight		string		`json:"weight"`
+    Enable              string      `json:"enabled"`
+    Hold                string      `json:"hold"`
+    Id                  string      `json:"id"`
+    Line                string      `json:"line"`
+    Line_id             string      `json:"line_id"`
+    Monitor_status      string      `json:"monitor_status"`
+    Mx                  string      `json:"mx"`
+    Name                string      `json:"name"`
+    Remark              string      `json:"remake"`
+    Status              string      `json:"status"`
+    Ttl                 string      `json:"ttl"`
+    Type                string      `json:"type"`
+    Updated_on          string      `json:"updated_on"`
+    Use_aqb             string      `json:"use_aqb"`
+    Value               string      `json:"value"`
+    Weight              string      `json:"weight"`
 }
 
 type Status struct {
-	Code		string		`json:"code"`
-	Create_at	string		`json:"create_at"`
-	Message		string		`json:"message"`
+    Code        string      `json:"code"`
+    Create_at   string      `json:"create_at"`
+    Message     string      `json:"message"`
 }
 
 type R_AddRecord struct {
-	Id		string		`json:"id"`
-	Name		string		`json:"name"`
-	Status		string		`json:"status"`
+    Id          string      `json:"id"`
+    Name        string      `json:"name"`
+    Status      string      `json:"status"`
 }
 
 type R_Status_Add struct {
-	Status		Status		`json:"status"`
-	Record		R_AddRecord	`json:"record"`
+    Status      Status      `json:"status"`
+    Record      R_AddRecord `json:"record"`
 }
 
 type R_Status_Remove struct {
-	Status		Status		`json:"status"`
+    Status      Status      `json:"status"`
 }
 
 type DomainList struct {
-	Domain		Domain		`json:"domain"`
-	Info		Info		`json:"info"`
-	Records		[]Record	`json:"records"`
-	Status		Status		`json:"status"`
+    Domain      Domain      `json:"domain"`
+    Info        Info        `json:"info"`
+    Records     []Record    `json:"records"`
+    Status      Status      `json:"status"`
 }
 
 
 func main() {
 
-	err := CheckRecord()
-	if err != nil {
-		fmt.Println("something err, ", err)
-	}
+    err := CheckRecord()
+    if err != nil {
+        fmt.Println("something err, ", err)
+    }
 
 }
 
 func CheckRecord() error {
 
-	WebDomainRecord, err := GetDomainRecords()
-	LBDomainRecord, err1 := net.LookupHost("aws-devops-demo-8082-8081-579254150.cn-northwest-1.elb.amazonaws.com.cn")
+    WebDomainRecord, err := GetDomainRecords()
+    LBDomainRecord, err1 := net.LookupHost("aws-devops-demo-8082-8081-579254150.cn-northwest-1.elb.amazonaws.com.cn")
 
-	if err != nil {
-		fmt.Println("Err WebDomainRecord: ", err.Error())
-		os.Exit(-1)
-	} else if err1 != nil {
-		fmt.Println("Err LBDomainRecord: ", err1.Error())
-		os.Exit(-2)
-	}
+    if err != nil {
+        fmt.Println("Err WebDomainRecord: ", err.Error())
+        os.Exit(-1)
+    } else if err1 != nil {
+        fmt.Println("Err LBDomainRecord: ", err1.Error())
+        os.Exit(-2)
+    }
 
-        var DelArray []string
-        var AddArray []string
+    var DelArray []string
+    var AddArray []string
 
-        for ip, id := range WebDomainRecord {
-                FlagExist := 0
-                for _, n := range LBDomainRecord {
-                        if n == ip {
-			        FlagExist = 1
-				break
-                        }
-		}
-                if FlagExist == 0 {
-                	err = RemoveDomainRecord(id) // record id
-                	if err != nil {
-                	        fmt.Println(err, "ip: ",ip)
-                	        continue
-                	}
-                	fmt.Println("del record success.../ value: ", ip)
-                        DelArray = append(DelArray, ip)
-		}
+    for ip, id := range WebDomainRecord {
+        FlagExist := 0
+        for _, n := range LBDomainRecord {
+                if n == ip {
+                    FlagExist = 1
+                    break
+                }
         }
+        if FlagExist == 0 {
+            err = RemoveDomainRecord(id) // record id
+            if err != nil {
+                fmt.Println(err, "ip: ",ip)
+                continue
+            }
+            fmt.Println("del record success.../ value: ", ip)
+            DelArray = append(DelArray, ip)
+        }
+    }
 
-	for _, n := range LBDomainRecord {
-		FlagNeedAdd := 1
-        	for ip, _ := range WebDomainRecord {
-			if n == ip {
-			        FlagNeedAdd = 0
-				break
-			}
-		}
-		if FlagNeedAdd == 1 {
-			err = CreateDomainRecord("@", "A", "0", n) // subname, type, line, value
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			fmt.Println("Add record success.../ value: ", n)
-			AddArray = append(AddArray, n)
-        	}
-	}
+    for _, n := range LBDomainRecord {
+        FlagNeedAdd := 1
+        for ip, _ := range WebDomainRecord {
+            if n == ip {
+                FlagNeedAdd = 0
+                break
+            }
+        }
+        if FlagNeedAdd == 1 {
+            err = CreateDomainRecord("@", "A", "0", n) // subname, type, line, value
+            if err != nil {
+                fmt.Println(err)
+                continue
+            }
+            fmt.Println("Add record success.../ value: ", n)
+            AddArray = append(AddArray, n)
+        }
+    }
 
-	fmt.Printf("Del %d record\n", len(DelArray))
-	fmt.Printf("Add %d record\n", len(AddArray))
+    fmt.Printf("Del %d record\n", len(DelArray))
+    fmt.Printf("Add %d record\n", len(AddArray))
 
-	return nil
+    return nil
 }
 
 func GetDomainRecords() (map[string]string, error) {
